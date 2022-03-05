be10540dcc432b0d72e40db3434dac4e0894c11e
Мухаммад <human.twm@gmail.com>
2022-03-05 20:13:04 +0300

    part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "/src/components/layout";
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>This site I'm making with Gatsby</p>
+        </Layout>
+    )
+}
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,15 @@
-import * as React from "react"
+import * as React from 'react'
 
-// styles
-const titleStyle = {
-  fontFamily: "monospace",
-  color: "#333",
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  fontSize: 40,
-}
-const mozyStyle = {
-  color: "#f11861",
-  fontWeight: 700,
-}
+import Layout from "/src/components/layout.js"
 
-// markup
 const IndexPage = () => {
   return (
-    <h1 style={titleStyle} class="title">What the hell is <span style={mozyStyle}>mozy</span>?</h1>
+    <Layout pageTitle="Home Page">
+      <p>I'm Muhammad from Anchih</p>
+    </Layout>
   )
 }
 
 export default IndexPage
+
+
