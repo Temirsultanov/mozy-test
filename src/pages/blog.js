4cb01f81efad34839f1bb46e6ff59fbd67f714a7
Мухаммад <human.twm@gmail.com>
2022-03-07 02:19:03 +0300

    Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,26 +1,44 @@
 import * as React from "react"
 import Layout from "/src/components/layout.js"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 import { graphql } from "gatsby"
 
+const postStyle = {
+    marginBottom: 50,
+}
+const postBodyStyle = {
+    padding: 20,
+    border: "1px solid black"
+
+}
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map(file => <li key={file.name}>{file.name}</li> )
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map(node => (
+                    <article style={postStyle} key={node.key}>
+                        <h3>{node.frontmatter.title}</h3>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <div style={postBodyStyle}><MDXRenderer>{ node.body }</MDXRenderer></div>
+                    </article>
+                ))
+            }
         </Layout>
     )
 };
 
-export const q = graphql`
+export const query = graphql`
     query {
-        allFile {
-            nodes {
-                name
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+        nodes {
+            frontmatter {
+                title
+                date(formatString: "MMMM, D, YYYY")
             }
+            id
+            body
+        }
         }
     }
 `;
